e496ce69fb2a774c86606b239ee2567703db2a1d
Thecabezon <jeffersonrojassosa150@gmail.com>
2024-09-15 15:41:15 -0500

    subir archivos, agregar tarea , index php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tareas</title>
+</head>
+<body>
+    <h1>Mis Tareas</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Tarea</th>
+            </tr>
+        </thead>
+    </table>
+</body>
+</html>
